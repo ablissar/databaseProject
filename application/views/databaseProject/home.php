d64d7644dab2911f4199d167ff31447b67f9d512
Adam Bliss <ablissar@gmail.com>
2017-04-20 18:08:48 -0500

    Initial MVC setup, created Home view

---
+++ application/views/databaseProject/home.php
@@ -0,0 +1,47 @@
+<head>
+    <title>Adam Bliss Project Home</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <td>Number</td>
+                <td>Function</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Add a new student.</td>
+                <td><input type='button' value='Go' /></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Add a new course.</td>
+                <td><input type='button' value='Go' /></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Add a new enrollment.</td>
+                <td><input type='button' value='Go' /></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>View all students.</td>
+                <td><input type='button' value='Go' /></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>View all courses from a given department.</td>
+                <td><input type='button' value='Go' /></td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Add ll courses for a given student.</td>
+                <td><input type='button' value='Go' /></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
