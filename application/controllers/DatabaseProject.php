3e5fbbf5e0ea33f617a8646f43af0df018b28810
Adam Bliss <ablissar@gmail.com>
2017-04-20 20:01:54 -0500

    Altered controller and home page to link to empty views

--- application/controllers/DatabaseProject.php
+++ application/controllers/DatabaseProject.php
@@ -5,9 +5,66 @@ class DatabaseProject extends CI_Controller {
 
 	public function index()
 	{
+        if($this->input->post('one')) {
+            log_message('debug', 'test_one');
+            redirect('/databaseProject/add_student');
+        } else if ($this->input->post('two')) {
+            redirect('/databaseProject/add_course');
+        } else if ($this->input->post('three')) {
+            redirect('/databaseProject/add_enrollment');
+        } else if ($this->input->post('four')) {
+            redirect('/databaseProject/view_students');
+        } else if ($this->input->post('five')) {
+            redirect('/databaseProject/view_courses_by_department');
+        } else if ($this->input->post('six')) {
+            redirect('/databaseProject/view_student_schedule');
+        }
+
 		$this->load->view('templates/header');
         $this->load->view('databaseProject/home');
         $this->load->view('templates/footer');
 	}
+
+    public function add_student()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('databaseProject/add_student');
+        $this->load->view('templates/footer');
+    }
+
+    public function add_course()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('databaseProject/add_course');
+        $this->load->view('templates/footer');
+    }
+
+    public function add_enrollment()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('databaseProject/add_enrollment');
+        $this->load->view('templates/footer');
+    }
+
+    public function view_students()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('databaseProject/view_students');
+        $this->load->view('templates/footer');
+    }
+
+    public function view_courses_by_department()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('databaseProject/view_courses_by_department');
+        $this->load->view('templates/footer');
+    }
+
+    public function view_student_schedule()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('databaseProject/view_student_schedule');
+        $this->load->view('templates/footer');
+    }
 }
  ?>
--- application/views/databaseProject/home.php
+++ application/views/databaseProject/home.php
@@ -2,36 +2,38 @@
     <header class="header">
         Adam Bliss Project Home <br />
     </header>
+    <?php echo form_open('', ''); ?>
     <table>
         <tr>
             <td>1</td>
             <td>Add a new student.</td>
-            <td><input type='button' value='Go' /></td>
+            <td><?php echo form_submit('one', 'Go'); ?></td>
         </tr>
         <tr>
             <td>2</td>
             <td>Add a new course.</td>
-            <td><input type='button' value='Go' /></td>
+            <td><?php echo form_submit('two', 'Go'); ?></td>
         </tr>
         <tr>
             <td>3</td>
             <td>Add a new enrollment.</td>
-            <td><input type='button' value='Go' /></td>
+            <td><?php echo form_submit('three', 'Go'); ?></td>
         </tr>
         <tr>
             <td>4</td>
             <td>View all students.</td>
-            <td><input type='button' value='Go' /></td>
+            <td><?php echo form_submit('four', 'Go'); ?></td>
         </tr>
         <tr>
             <td>5</td>
             <td>View all courses from a given department.</td>
-            <td><input type='button' value='Go' /></td>
+            <td><?php echo form_submit('five', 'Go'); ?></td>
         </tr>
         <tr>
             <td>6</td>
             <td>View all courses for a given student.</td>
-            <td><input type='button' value='Go' /></td>
+            <td><?php echo form_submit('six', 'Go'); ?></td>
         </tr>
     </table>
+    <?php echo form_close(); ?>
 </body>
