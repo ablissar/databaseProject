896e3bfa60f7f6918ffba4887ccef8ee32bb3def
Adam Bliss <ablissar@gmail.com>
2017-04-21 23:40:05 -0500

    Cleaned up structure of home page

--- application/views/databaseProject/home.php
+++ application/views/databaseProject/home.php
@@ -1,67 +1,51 @@
 <body>
-    <header class="header">
-        Adam Bliss Project Home <br />
+    <header>
+        <h1 class="header">Adam Bliss Project Home</h1>
     </header>
     <?php
         echo form_open('', '');
+        $data = array(
+            'name'      => 'add_student',
+            'value'     => 'Add a student.',
+            'class'     => 'nav_button'
+        );
+        echo form_submit($data);
 
+        $data = array(
+            'name'      => 'add_course',
+            'value'     => 'Add a course.',
+            'class'     => 'nav_button'
+        );
+        echo form_submit($data);
+
+        $data = array(
+            'name'      => 'add_enrollment',
+            'value'     => 'Enroll a student in a course.',
+            'class'     => 'nav_button'
+        );
+        echo form_submit($data);
+
+        $data = array(
+            'name'      => 'view_students',
+            'value'     => 'View all students.',
+            'class'     => 'nav_button'
+        );
+        echo form_submit($data);
+
+        $data = array(
+            'name'      => 'view_courses_by_department',
+            'value'     => 'View all courses for a given department.',
+            'class'     => 'nav_button'
+        );
+        echo form_submit($data);
+
+        $data = array(
+            'name'      => 'view_student_schedule',
+            'value'     => 'View all courses for a given student.',
+            'class'     => 'nav_button'
+        );
+        echo form_submit($data);
+
+        echo form_close();
     ?>
-
-
-
-                <?php
-                    $data = array(
-                        'name'      => 'add_student',
-                        'value'     => 'Add a student.',
-                        'class'     => 'nav_button'
-                    );
-                    echo form_submit($data);
-                ?>
-
-                <?php
-                    $data = array(
-                        'name'      => 'add_course',
-                        'value'     => 'Add a course.',
-                        'class'     => 'nav_button'
-                    );
-                    echo form_submit($data);
-                ?>
-
-                <?php
-                    $data = array(
-                        'name'      => 'add_enrollment',
-                        'value'     => 'Enroll a student in a course.',
-                        'class'     => 'nav_button'
-                    );
-                    echo form_submit($data);
-                ?>
-
-                <?php
-                    $data = array(
-                        'name'      => 'view_students',
-                        'value'     => 'View all students.',
-                        'class'     => 'nav_button'
-                    );
-                    echo form_submit($data);
-                ?>
-
-                <?php
-                    $data = array(
-                        'name'      => 'view_courses_by_department',
-                        'value'     => 'View all courses for a given department.',
-                        'class'     => 'nav_button'
-                    );
-                    echo form_submit($data);
-                ?>
-
-                <?php
-                    $data = array(
-                        'name'      => 'view_student_schedule',
-                        'value'     => 'View all courses for a given student.',
-                        'class'     => 'nav_button'
-                    );
-                    echo form_submit($data);
-                ?>
-
-    <?php echo form_close(); ?>
 </body>
