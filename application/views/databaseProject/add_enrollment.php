cd591be70edb10feafcd9a4ccd86dc9d7ecd3e8f
Adam Bliss <ablissar@gmail.com>
2017-04-22 13:18:13 -0500

    Added input boxes to add_enrollment view

--- application/views/databaseProject/add_enrollment.php
+++ application/views/databaseProject/add_enrollment.php
@@ -1,5 +1,19 @@
 <body>
-    <?php echo form_open('', ''); ?>
-    <?php echo form_submit('home', 'Home Page'); ?>
-    <?php echo form_close(); ?>
+    <?php
+        echo form_open('databaseProject/add_enrollment');
+
+        echo "New Department Code: ";
+        echo form_input('new_department_code');
+
+        echo "New Course Number: ";
+        echo form_input('new_course_number');
+
+        echo "New Student ID: ";
+        echo form_input('new_student_id');
+
+        echo form_submit('submit', 'Add Course');
+        echo form_submit('home', 'Home Page');
+        echo form_close();
+        echo $status;
+    ?>
 </body>
