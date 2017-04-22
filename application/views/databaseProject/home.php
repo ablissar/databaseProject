646698cf7e94c6776924893b422dcf2808f45c4c
Adam Bliss <ablissar@gmail.com>
2017-04-22 11:56:26 -0500

    Centered buttons, still need to work on coloring

--- application/views/databaseProject/home.php
+++ application/views/databaseProject/home.php
@@ -2,52 +2,54 @@
     <header>
         Adam Bliss Project Home
     </header>
-    <div class="nav_buttons">
-    <?php
-        echo form_open('', '');
-        $data = array(
-            'name'      => 'add_student',
-            'value'     => 'Add a student.',
-            'class'     => 'nav_button'
-        );
-        echo form_submit($data);
+    <div class="center_column">
+        <div class="nav_buttons">
+        <?php
+            echo form_open('', '');
+            $data = array(
+                'name'      => 'add_student',
+                'value'     => 'Add a student.',
+                'class'     => 'nav_button'
+            );
+            echo form_submit($data);
 
-        $data = array(
-            'name'      => 'add_course',
-            'value'     => 'Add a course.',
-            'class'     => 'nav_button'
-        );
-        echo form_submit($data);
+            $data = array(
+                'name'      => 'add_course',
+                'value'     => 'Add a course.',
+                'class'     => 'nav_button'
+            );
+            echo form_submit($data);
 
-        $data = array(
-            'name'      => 'add_enrollment',
-            'value'     => 'Enroll a student in a course.',
-            'class'     => 'nav_button'
-        );
-        echo form_submit($data);
+            $data = array(
+                'name'      => 'add_enrollment',
+                'value'     => 'Enroll a student in a course.',
+                'class'     => 'nav_button'
+            );
+            echo form_submit($data);
 
-        $data = array(
-            'name'      => 'view_students',
-            'value'     => 'View all students.',
-            'class'     => 'nav_button'
-        );
-        echo form_submit($data);
+            $data = array(
+                'name'      => 'view_students',
+                'value'     => 'View all students.',
+                'class'     => 'nav_button'
+            );
+            echo form_submit($data);
 
-        $data = array(
-            'name'      => 'view_courses_by_department',
-            'value'     => 'View courses by department.',
-            'class'     => 'nav_button'
-        );
-        echo form_submit($data);
+            $data = array(
+                'name'      => 'view_courses_by_department',
+                'value'     => 'View courses by department.',
+                'class'     => 'nav_button'
+            );
+            echo form_submit($data);
 
-        $data = array(
-            'name'      => 'view_student_schedule',
-            'value'     => 'View student schedule.',
-            'class'     => 'nav_button'
-        );
-        echo form_submit($data);
+            $data = array(
+                'name'      => 'view_student_schedule',
+                'value'     => 'View student schedule.',
+                'class'     => 'nav_button'
+            );
+            echo form_submit($data);
 
-        echo form_close();
-    ?>
+            echo form_close();
+        ?>
+        </div>
     </div>
 </body>
