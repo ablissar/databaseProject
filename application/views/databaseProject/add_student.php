01433df524afba5340e71648828dbb45d6772948
Adam Bliss <ablissar@gmail.com>
2017-04-22 12:00:21 -0500

    Added input boxes to add_student view

--- application/views/databaseProject/add_student.php
+++ application/views/databaseProject/add_student.php
@@ -1,5 +1,13 @@
 <body>
-    <?php echo form_open('', ''); ?>
-    <?php echo form_submit('home', 'Home Page'); ?>
-    <?php echo form_close(); ?>
+    <?php
+        echo form_open('', '');
+        echo "New Student ID: ";
+        echo form_input('new_student_id');
+        echo "New Student Name: ";
+        echo form_input('new_student_name');
+        echo "New Student Major: ";
+        echo form_input('new_student_major');
+        echo form_submit('home', 'Home Page');
+        echo form_close();
+    ?>
 </body>
