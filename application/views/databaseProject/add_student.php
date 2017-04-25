b863317953fd2eaf94ef6420e7fec39b34e2dcd7
Adam Bliss <ablissar@gmail.com>
2017-04-24 20:10:26 -0500

    Styling for add_student page

--- application/views/databaseProject/add_student.php
+++ application/views/databaseProject/add_student.php
@@ -1,15 +1,38 @@
 <body>
-    <?php
-        echo form_open('databaseProject/add_student');
-        echo "New Student ID: ";
-        echo form_input('new_student_id');
-        echo "New Student Name: ";
-        echo form_input('new_student_name');
-        echo "New Student Major: ";
-        echo form_input('new_student_major');
-        echo form_submit('submit', 'Add Student');
-        echo form_submit('home', 'Home Page');
-        echo form_close();
-        echo $status;
-    ?>
+    <header><?php echo $title; ?></header>
+    <div class='form'>
+        <?php echo form_open('databaseProject/add_student'); ?>
+        <div class='form_entry'>
+            <?php
+            echo "New Student ID: ";
+            echo form_input('new_student_id');
+            ?>
+        </div>
+        <div class='form_entry'>
+            <?php
+            echo "New Student Name: ";
+            echo form_input('new_student_name');
+            ?>
+        </div>
+        <div class='form_entry'>
+            <?php
+            echo "New Student Major: ";
+            echo form_input('new_student_major');
+            ?>
+        </div>
+    </div>
+    <div class='form_buttons'>
+        <?php
+            echo form_submit(array(
+                'name' => 'submit',
+                'value' => 'Add Student',
+                'class' => 'form_submit_button'));
+            echo form_submit(array(
+                'name' => 'home',
+                'value' => 'Home Page',
+                'class' => 'home_page_button'));
+            echo form_close();
+            echo $status;
+        ?>
+    </div>
 </body>
